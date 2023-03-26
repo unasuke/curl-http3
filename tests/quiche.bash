12b87bf1cd1d522e013fda3dd62bd30ad830104b
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-03-26 10:09:00 +0900

    Use quic.nginx.org for test website

--- tests/quiche.bash
+++ tests/quiche.bash
@@ -5,4 +5,5 @@ set -eux
 docker pull ghcr.io/unasuke/curl-http3:quiche-latest
 
 docker run --rm ghcr.io/unasuke/curl-http3:quiche-latest /usr/local/bin/curl --http3 https://www.google.com/
-docker run --rm ghcr.io/unasuke/curl-http3:quiche-latest /usr/local/bin/curl --http3 https://nghttp2.org:4433/
+# docker run --rm ghcr.io/unasuke/curl-http3:quiche-latest /usr/local/bin/curl --http3 https://nghttp2.org:4433/
+docker run --rm ghcr.io/unasuke/curl-http3:quiche-latest /usr/local/bin/curl --http3 https://quic.nginx.org
